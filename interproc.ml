5434bd12323dc55a328183aca0b571bbbae69cbb
Josselin Giet <josselin.giet@inria.fr>
2023-10-17 14:56:09 +0200

    Interproc: using module Options to avoid conflict with Option module introduced in Ocaml 4.08

--- interproc.ml
+++ interproc.ml
@@ -3,37 +3,37 @@
 (* This file is part of the Interproc analyzer, released under GPL license.
    Please read the COPYING file packaged in the distribution.
 
-   Copyright (C) Mathias Argoud, Gaël Lalire, Bertrand Jeannet 2007.
+   Copyright (C) Mathias Argoud, Gaï¿½l Lalire, Bertrand Jeannet 2007.
 *)
 
 open Format
-open Option
+open Options
 
 let main () =
   (* Parsing the command line *)
   Arg.parse
-    Option.speclist
-      (begin fun name -> Option.inputfilename := name end)
+    speclist
+      (begin fun name -> Options.inputfilename := name end)
       "interproc <options> <inputfile>"
   ;
   (* Parsing the program *)
-  let input = open_in !Option.inputfilename in
+  let input = open_in !Options.inputfilename in
   let lexbuf = Lexing.from_channel input in
   lexbuf.Lexing.lex_curr_p <- { lexbuf.Lexing.lex_curr_p with
-    Lexing.pos_fname = "file "^(!Option.inputfilename);
+    Lexing.pos_fname = "file "^(!Options.inputfilename);
   };
   let prog = Frontend.parse_lexbuf Format.err_formatter lexbuf in
   close_in input;
   
   if !debug>0 then
     printf "%sProgram with its control points:%s@.%a@."
-      (!Option.displaytags).precolorB (!Option.displaytags).postcolor
+      (!Options.displaytags).precolorB (!Options.displaytags).postcolor
       (PSpl_syn.print_program
 	(begin fun fmt point ->
 	  fprintf fmt "%s%a%s"
-	  (!Option.displaytags).precolorR
+	  (!Options.displaytags).precolorR
 	  PSpl_syn.print_point point
-	  (!Option.displaytags).postcolor
+	  (!Options.displaytags).postcolor
 	end))
       prog
   ;
